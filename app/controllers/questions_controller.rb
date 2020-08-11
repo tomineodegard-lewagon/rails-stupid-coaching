cc97cd875f590522ca8ee538420569700a3eadc8
Tomine Odegard <tomineodegard@hotmail.no>
2020-08-11 21:25:33 +0200

    stupid coaching displaying correct answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,24 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+      if @question == "I am going to work"
+        @answer = "Great!"
+      elsif @question.include? "?"
+        @answer = "Silly question, get dressedand go to work!"
+      else
+        @answer = "I don´t care, get dressed and go to work!"
+    end
   end
-
 end
+
+
+
+
+
+
+
+
+
+
+
